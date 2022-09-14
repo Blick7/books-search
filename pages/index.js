176c0b0ca78eb5e03d91d0c305ed8b594e352e45
Blick <dima.8m8@gmail.com>
2022-09-14 19:26:11 +0300

    feat: add basic nextjs setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Books Search</title>
+        <meta name="description" content="Search books" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
